0055da49d0d9d60aa6e53f39823cf01851cb86ea
Jose-Dell22 <jfdell41@gmail.com>
2025-06-30 10:59:57 -0500

    Pruebas de api

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
+
 const express = require('express');
+const bodyparser= require('body-parser');
+
 const app = express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
 
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
@@ -10,6 +16,42 @@ app.get('/', (req, res) => {
         <h1>express.js </h1>
         <p>this is a express js course</p>
         <p>PORT: ${PORT}</p>`);
+});
+app.get('/user/:id', (req,res)=>{
+    const userId= req.params.id;
+    res.send(`Show user info by ID: ${userId}`)
+})
+app.get('/search',(req,res)=>{
+    const terms= req.query.term || 'no específicated';
+    const category =req.query.category || 'all';
+    res.send(`
+        <h2>search results:</h2>
+        <p>term: ${terms}</p>
+        <p>category: ${category}</p>`
+    );
+});
+app.post('/form', (req,res)=>{
+    const name = req.body.name || 'anonymus';
+    const email = req.body.email || 'N/A';
+    res.json({
+        message: 'Data received',
+        data: {
+            name,
+            email
+
+        }
+    })
+});
+
+app.post('/api/data', (req, res)=>{
+    const data = req.body;
+    if(!data || Object.keys(data).length===0){
+        return res.status(400).json({error: "No data was received"});
+    }
+    res.status(201).json({
+        message: "Json Data received",
+        data
+    })
 });
 
 app.listen(PORT, () => {
